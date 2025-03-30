2a91e503be31b04bbb7d12ba3899e759231c7508
Navya <navya.jaya19@gmail.com>
2025-03-30 19:02:27 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 # Create an S3 Bucket
-resource "aws_s3_bucket" "example_bucket1" {
+resource "aws_s3_bucket" "example_bucket" {
   bucket = "3tierswiggy-bucket-terraform-hcp"
 
   # Enable versioning
